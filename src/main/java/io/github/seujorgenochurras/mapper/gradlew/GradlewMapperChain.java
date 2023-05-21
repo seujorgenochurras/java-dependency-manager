fac27a150941b4dac50374a18f1bdc431171de44
Jorge Seu Churrasco com <eusoulegaldemaiseverdadeeujuro@gmail.com>
2023-05-21 17:24:15 -0300

    Added validator

--- src/main/java/io/github/seujorgenochurras/mapper/gradlew/GradlewMapperChain.java
+++
@@ -1,6 +0,0 @@
-package io.github.seujorgenochurras.mapper.gradlew;
-
-public class GradlewMapperChain {
-   private GradlewMapperChain(){}
-
-}
--- src/main/java/io/github/seujorgenochurras/mapper/gradlew/GradlewMapperValidator.java
+++
@@ -1,5 +0,0 @@
-package io.github.seujorgenochurras.mapper.gradlew;
-
-public interface GradlewMapperValidator {
-
-}
